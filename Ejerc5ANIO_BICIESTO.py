e0a6f3b65bc6f324433fa5133786f4f1ffdadd8c
Federico Falcon <ffalcondepizzo@gmail.com>
2022-05-05 17:47:58 -0300

    borre todo

--- Ejerc5ANIO_BICIESTO.py
+++ Ejerc5ANIO_BICIESTO.py
@@ -2,7 +2,7 @@
 #. Especifique una expresión condicional que, a partir de un año, determine si es bisiesto o no. 
 
 print("PROGRAMA ANIO BISIESTO")
-print("PRINT PARA VER COMMITS")
+
 anio = int(input("Ingrese anio a determinar: "))
 
 if ((anio % 4 == 0 and anio % 100 != 0) or (anio % 100 == 0 and anio % 400 == 0)):
